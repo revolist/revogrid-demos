5bb68329704907b2e0ec8cb134ca60bccfd8ccea
maks <kudrytskimaksim@gmail.com>
2026-08-19 01:27:38 +0100

    up

--- pro-filtering/src/filtering.test.mjs
+++ pro-filtering/src/filtering.test.mjs
@@ -89,7 +89,7 @@ test('all framework examples bind filter badges declaratively', async () => {
   const packageJson = JSON.parse(await readSource('../package.json'));
   assert.equal(
     packageJson.dependencies['@revolist/revogrid-pro'],
-    'npm:@revolist/rv-pro-trial@2.7.12',
+    'npm:@revolist/rv-pro-trial@2.7.13',
   );
 
   const styles = await readSource('filtering.scss');
